4a8067b2385bab2add02091d41d4aa65c3a83646
eleefoley <eleefoley@gmail.com>
2020-04-25 19:13:17 -0400

    the most basic prompt for a valid username and password works correctly

--- erp_app/user_prompt.py
+++ erp_app/user_prompt.py
@@ -2,15 +2,16 @@ import sys
 import pandas as pd
 
 user_info = pd.read_csv("Data/unpwd.csv")
-print(user_info.columns.values)
 
-un=""
-pw = ""
-while un=="" or pw =="":
+login = False
+while login == False:
 	un = input("Enter your username: ")
 	print(un)
-
-	pw = input("Enter your password: ")
-	print(pw)
+	if un in list(user_info['userID']): 
+		pw = input("Enter your password: ")
+		if user_info.query('userID == "' + str(un) + '"')['pw'][0] == pw:
+			login = True
+	else:
+		print("User name not found.  Try again.")
 
 print("All done")
