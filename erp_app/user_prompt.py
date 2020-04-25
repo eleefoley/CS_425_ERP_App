b8dc30747f13a0f76c7acd4776b3604f2c8f12b9
eleefoley <eleefoley@gmail.com>
2020-04-25 15:32:35 -0400

    simple script for prompting user

---
+++ erp_app/user_prompt.py
@@ -0,0 +1,14 @@
+import sys
+import pandas as pd
+
+
+un=""
+pw = ""
+while un=="" or pw =="":
+	un = input("Enter your username: ")
+	print(un)
+
+	pw = input("Enter your password: ")
+	print(pw)
+
+print("All done")
