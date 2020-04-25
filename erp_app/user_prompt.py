6ab4157ecd42a432eea57bedd7bc25cf91e67309
eleefoley <eleefoley@gmail.com>
2020-04-25 19:35:32 -0400

    added a function for login

--- erp_app/user_prompt.py
+++ erp_app/user_prompt.py
@@ -1,17 +1,19 @@
 import sys
 import pandas as pd
 
-user_info = pd.read_csv("Data/unpwd.csv")
+def login_user():
+	user_info = pd.read_csv("Data/unpwd.csv")
 
-login = False
-while login == False:
-	un = input("Enter your username: ")
-	print(un)
-	if un in list(user_info['userID']): 
-		pw = input("Enter your password: ")
-		if user_info.query('userID == "' + str(un) + '"')['pw'][0] == pw:
-			login = True
-	else:
-		print("User name not found.  Try again.")
+	login = False
+	while login == False:
+		un = input("Enter your username: ")
+		print(un)
+		if un in list(user_info['userID']): 
+			pw = input("Enter your password: ")
+			if user_info.query('userID == "' + str(un) + '"')['pw'][0] == pw:
+				login = True
+		else:
+			print("User name not found.  Try again.")
 
-print("All done")
+login_user()
+
