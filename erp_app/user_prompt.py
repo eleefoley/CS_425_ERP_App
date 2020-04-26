f435394851f776b2b635880cc7dfe368ff956b0a
eleefoley <eleefoley@gmail.com>
2020-04-26 12:01:43 -0400

    exits after 4th incorrect attempt

--- erp_app/user_prompt.py
+++ erp_app/user_prompt.py
@@ -3,17 +3,20 @@ import pandas as pd
 
 def login_user():
 	user_info = pd.read_csv("Data/unpwd.csv")
-
 	login = False
-	while login == False:
+	i = 0
+	while login == False and i <=3:
 		un = input("Enter your username: ")
 		print(un)
 		if un in list(user_info['userID']): 
 			pw = input("Enter your password: ")
 			if user_info.query('userID == "' + str(un) + '"')['pw'][0] == pw:
 				login = True
+			else:
+				print("Incorect password. ")
+				
 		else:
 			print("User name not found.  Try again.")
-
+		i = i + 1
 login_user()
 
