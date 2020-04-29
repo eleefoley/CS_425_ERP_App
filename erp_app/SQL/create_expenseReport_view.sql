ada4e763563d55efc3bfe16d068e5c3190c76f3f
eleefoley <eleefoley@gmail.com>
2020-04-28 23:38:51 -0400

    created a view and a select statement for admin four report

---
+++ erp_app/SQL/create_expenseReport_view.sql
@@ -0,0 +1,12 @@
+--Expense report, employee showing salary, bonus expense and part cost
+create or replace view expenseReport as (
+	SELECT 
+		cost,
+		'parts' as cost_type
+	FROM inventory
+	union
+	select
+		salary as cost,
+		'salary' as cost_type
+	from employee
+)
